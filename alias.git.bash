3d81aa11281e38448276ccb2b67331914dae7405
Robert Starbuck <robert.starbuck@buddi.co.uk>
2016-07-06 15:52:04 +0100

    Alias for getting the git hash has been included

--- alias.git.bash
+++ alias.git.bash
@@ -1,2 +1,4 @@
 git config --global alias.a '!git config -l | grep alias'
+git config --global alias.cm 'commit -m'
 git config --global alias.s 'status'
+git config --global alias.h '!git log -1 --format="%H"'
