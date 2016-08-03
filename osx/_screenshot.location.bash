3c39caca1ddd1e0c4d57840253a160f1a729595a
Robert Starbuck <robert.starbuck@buddi.co.uk>
2016-08-03 15:34:15 +0100

    Bookmarks added, osx/_screenshot.location.bash underscored (commented out)

--- osx/_screenshot.location.bash
+++ osx/_screenshot.location.bash
@@ -1,4 +1,5 @@
 #!/bin/bash
+# @BUG There's something wrong here, the function seems to fire when bash activity is occuring
 ss-location(){	
 	defaults write com.apple.screencapture location "${PWD}";killall SystemUIServer
 }
